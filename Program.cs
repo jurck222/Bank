9b44ca17fc54f275761e94fc576a7cb54cada7d0
jurck222 <56190152+jurck222@users.noreply.github.com>
2022-10-21 12:24:57 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using vaja01;
+
+var account = new BankAccount("Jure", 1000);
+
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+/*BankAccount invalidAccount;
+try
+{
+    invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+    return;
+}
+try
+{
+    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}*/
+Console.WriteLine(account.GetAccountHistory());
